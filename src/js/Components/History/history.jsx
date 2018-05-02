10de32cb54cf06324375c44366ce37f55885f6c4
Steve Schwarzmann <steven.schwarzmann@gmail.com>
2018-05-02 15:46:49 -0700

    History component functional along with search bar

--- src/js/Components/History/history.jsx
+++ src/js/Components/History/history.jsx
@@ -10,17 +10,10 @@ export default class History extends React.Component{
                 <header className='bg-primary'> Search History </header>
                 <div>
                     <ul id='historyList'>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                        <li>5</li>
-                        <li>6</li>
-                        <li>7</li>
-                        <li>8</li>
-                        <li>9</li>
-                        <li>10</li>
-                        <li>11</li>
+                    <div key={Date.now}>
+              <p>{ this.props.history }</p>
+              <p>{ this.props.history.date }</p>
+              </div>
                     </ul>
                 </div>
            </div>
