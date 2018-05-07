ea02c6535ab2d88ef7d873484f5c57ca8d9a3de6
Steve Schwarzmann <steven.schwarzmann@gmail.com>
2018-05-07 08:36:02 -0700

    Port changed for heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 /* eslint no-console: "off" */
 const server = require('./server');
 const express = require('express');
+require('dotenv').config()
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
